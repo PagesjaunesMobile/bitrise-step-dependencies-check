1710415f1789b2b4d776ff1ea8136143822ab133
CyrilPatrice <cpatrice@pagesjaunes.fr>
2018-10-16 14:57:58 +0200

    feat(diff) : specify a branch
    show already merged differences

--- step.sh
+++ step.sh
@@ -4,5 +4,5 @@ set -v
 THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
-depDiff=$(git difftool -y -x "diff -y -w" ext.gradle 2>&1  | grep "|" | tr -d '[:blank:]' | sed 's/|.*[:=]/ /' | ruby $THIS_SCRIPTDIR/compileDep.rb) 
+depDiff=$(git difftool -y -x "diff -y -w" $REF_BRANCH -- ext.gradle 2>&1  | grep "|" | tr -d '[:blank:]' | sed 's/|.*[:=]/ /' | ruby $THIS_SCRIPTDIR/compileDep.rb) 
 envman add --key PJ_DEP_DIFF --value "$depDiff"
